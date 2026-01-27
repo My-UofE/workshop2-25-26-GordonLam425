ac2789ea5c93130db469163c6fc9fbf0a5682a89
GordonLam425 <ll745@exeter.ac.uk>
2026-01-27 22:08:15 +0000

    adding the source code

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,3 +1,8 @@
 // Add your code below
-
+// HelloWorld application
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.ptintln("HelloWorld!");
+    }
+}
 
