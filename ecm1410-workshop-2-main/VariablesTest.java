9a1b015cc36a0cfb9a7102bd913d71417ad5081f
GordonLam425 <ll745@exeter.ac.uk>
2026-01-30 13:25:37 +0000

    try displaying the value

--- ecm1410-workshop-2-main/VariablesTest.java
+++ ecm1410-workshop-2-main/VariablesTest.java
@@ -1,9 +1,15 @@
 // VariablesTest application
 public class VariablesTest {
     public static void main(String[] args) {
-        byte s_per_min = 60;
-        long s_per_day = (s_per_min * 60 * 24);
-        System.out.println("Seconds per minute: " + s_per_min);
-        System.out.println("Seconds per day: " + s_per_day);
+        float area = 20.3F;
+        char a = '\u5468', b = '\\';
+        int nHours = 022;
+        double ratio = 7/5;
+
+        System.out.println("area: " + area);
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("nHours: " + nHours);
+        System.out.println("ratio: " + ratio);
     }
 }
